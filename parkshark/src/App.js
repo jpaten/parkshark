c6012ce7c34da734a9ccbc3c9a537d378c2e4002
drakecote <79180618+drakecote@users.noreply.github.com>
2022-10-06 15:02:24 -0700

    initial react set up

---
+++ parkshark/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Window from './components/Window';
+
+function App() {
+  return (
+    <div className="App">
+      <Window />
+    </div>
+  );
+}
+
+export default App;
+
