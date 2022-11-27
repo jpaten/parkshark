cb6abf163f324a5ec114db6ab3c051dbf7ab4946
jpaten <jpaten@ucla.edu>
2022-11-27 13:18:41 -0800

    Added user detection and geocoding to new user page, fixed bugs in Listing page (#26)
    * Got geocoding working properly
    
    * Added user detection to newListingForm
    
    * Added null value correction to SpotCard

--- parkshark/src/components/Listings/SpotCard.js
+++ parkshark/src/components/Listings/SpotCard.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 
@@ -18,27 +18,65 @@ const ResultCard = styled(Card)(({ theme }) => ({
     backgroundColor: 'rgb(245,245,250)'
 }))
 
+
+
 const SpotCard = ({result}) => {
+
+    const [nullCorrectedAddress, setNullCorrectedAddress] = useState({});
+
+    useEffect(() => {
+        if(!result.address){
+            setNullCorrectedAddress({
+                city: "Unknown City",
+                state: "Unknown State",
+                line_1: "Unknown Address",
+                line_2: "",
+            })
+        }
+
+        else {
+            const fixedAddress = {
+                city: result.address.city ?? "Unknown City",
+                state: result.address.state ?? "Unknown State",
+                line_1: result.address.line_1 ?? "",
+                line_2: result.address.line_2 ?? "",
+            };
+            setNullCorrectedAddress(fixedAddress);
+            console.log(fixedAddress);
+        }
+    }, [])
+
     return (
         <Grid item xs={12} sm={6} md={4}>
-            <ResultCard>
-                <CardActionArea>
-                    <CardMedia component="img" height="294" image={result.image} alt="Property Image" />
-                    <CardContent height="200px">
-                        <Typography gutterBottom variant="subtitle1" component="div" textAlign="left">
-                            {result.address.line_1}, {result.address.city}
+            <a href={`/listing/${result._id}`}>
+                <ResultCard>
+                    <CardActionArea>
+                        <CardMedia component="img" height="294" image={"https://images.pexels.com/photos/170811/pexels-photo-170811.jpeg?auto=compress&cs=tinysrgb&w=1600"} alt="Property Image" />
+                        <CardContent height="200px">
+                            <Typography gutterBottom variant="subtitle1" component="div" textAlign="left">
+                                {nullCorrectedAddress.city}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary" textAlign="left">
+                                {nullCorrectedAddress.line_1}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary" textAlign="left">
+                                {nullCorrectedAddress.line_2}
+                            </Typography>
+                            <Typography variant="body2" color="text.secondary" textAlign="left">
+                                {result.description ?? "A most excellent parking spot"}
+                            </Typography>
+                            <Typography variant="body2" color="text.primary" textAlign="left">
+                                {result.availability.length > 0 ? `${new Date(result.availability[0].start_time).toLocaleDateString('en-US', { timeZone: 'UTC', month: 'short', day: 'numeric', year: 'numeric' })} - ${new Date(result.availability[0].end_time).toLocaleDateString('en-US', { timeZone: 'UTC', month: 'short', day: 'numeric', year: 'numeric' } )}` : "Spot not available"}
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                    <div style={{float: "left"}}>
+                        <Typography variant="button" color="text.secondary" style={{position: "absolute", bottom: "2px"}} >
+                            &#36;{result.price} / Hour
                         </Typography>
-                        <Typography variant="body2" color="text.secondary" textAlign="left">
-                            {result.description}
-                        </Typography>
-                    </CardContent>
-                </CardActionArea>
-                <div style={{float: "left"}}>
-                    <Typography variant="button" color="text.secondary" style={{position: "absolute", bottom: "2px"}} >
-                        &#36;{result.price} / Day
-                    </Typography>
-                </div>
-            </ResultCard>
+                    </div>
+                </ResultCard>
+            </a>
         </Grid>
     )
 }
